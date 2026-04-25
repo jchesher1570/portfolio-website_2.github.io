ee32f855eb6df6cc947b9d8a9d8fe41a51f7746e
jchesher92 <jchesher92@gmail.com>
2026-04-25 14:35:29 +0000

    h1 names vertical stack

--- scripts/title_animation.js
+++ scripts/title_animation.js
@@ -1,7 +1,15 @@
 const h1 = document.querySelector("#hero h1");
+const [firstName, lastName] = h1.textContent.split(" ");
 const text = h1.textContent;
 
 h1.innerHTML = text
+  <div class="name-line first">
+    ${firstName.split("").map(l => `<span class="letter">${l}</span>`).join("")}
+  </div>
+  <div class="name-line last">
+    ${lastName.split("").map(l => `<span class="letter">${l}</span>`).join("")}
+  </div> `
+
   .split("")
   .map(letter => `<span class="letter">${letter === " " ? "&nbsp;" : letter}</span>`)
   .join("");
