e203add931fe94e2259ee6796dedaaef8ea316b9
jchesher92 <jchesher92@gmail.com>
2026-04-25 16:22:49 +0000

    Gradual reveal of each letter in the h1

--- scripts/title_animation.js
+++ scripts/title_animation.js
@@ -5,12 +5,16 @@ const [firstName, lastName] = h1.textContent.trim().split(" ");
 h1.innerHTML = `
   <div class="name-line first">
     ${firstName.split("").map((l, i) => 
-      `<span class="letter" style="animation-delay:${i * 0.05}s">${l}</span>`
+      `<span class="letter-wrap">
+         <span class="letter" style="animation-delay:${i * 0.06}s">${l}</span>
+       </span>`
     ).join("")}
   </div>
   <div class="name-line last">
     ${lastName.split("").map((l, i) => 
-      `<span class="letter" style="animation-delay:${(i + firstName.length) * 0.05}s">${l}</span>`
+      `<span class="letter-wrap">
+         <span class="letter" style="animation-delay:${(i + firstName.length) * 0.06}s">${l}</span>
+       </span>`
     ).join("")}
   </div>
 `;
