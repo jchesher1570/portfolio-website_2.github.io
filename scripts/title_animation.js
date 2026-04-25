b7a771722263b3b2fdca32dbe67cdf6eace231f3
jchesher92 <jchesher92@gmail.com>
2026-04-25 14:42:56 +0000

    title animation fix 2

--- scripts/title_animation.js
+++ scripts/title_animation.js
@@ -1,13 +1,16 @@
 const h1 = document.querySelector("#hero h1");
-const [firstName, lastName] = h1.textContent.split(" ");
-const text = h1.textContent;
 
-h1.innerHTML = text
+const [firstName, lastName] = h1.textContent.trim().split(" ");
+
+h1.innerHTML = `
   <div class="name-line first">
-    ${firstName.split("").map(l => `<span class="letter">${l}</span>`).join("")}
+    ${firstName.split("").map((l, i) => 
+      `<span class="letter" style="animation-delay:${i * 0.05}s">${l}</span>`
+    ).join("")}
   </div>
   <div class="name-line last">
-    ${lastName.split("").map(l => `<span class="letter">${l}</span>`).join("")}
-  </div> 
-  `;
-
+    ${lastName.split("").map((l, i) => 
+      `<span class="letter" style="animation-delay:${(i + firstName.length) * 0.05}s">${l}</span>`
+    ).join("")}
+  </div>
+`;
