6273b92524c2eeed387c85d615db54328e26d521
jchesher92 <jchesher92@gmail.com>
2026-05-17 17:02:13 +0000

    Changing opacity for active and nonactive links

---
+++ scripts/active_link.js
@@ -0,0 +1,18 @@
+<script>
+  const navLinks = document.querySelectorAll(
+    ".nav_link_home, .nav_link"
+  );
+
+  navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+
+      /* Remove active class from all links */
+      navLinks.forEach(item => {
+        item.classList.remove("active");
+      });
+
+      /* Add active class to clicked link */
+      link.classList.add("active");
+    });
+  });
+</script>
